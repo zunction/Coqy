7b8b976a12ecd07c9a0cd1de83c2f5f0cf9568e1
Zhangsheng Lai <dunno.noe@gmail.com>
2016-11-22 12:50:49 +0800

    stopped at Theorem mult_0_r

--- cis500/Induction.v
+++ cis500/Induction.v
@@ -130,6 +130,10 @@ Proof.
 Theorem mult_0_r : forall n:nat,
   n * 0 = 0.
 Proof.
+  induction n as [|n' IHn'].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
   (* FILL IN HERE *) Admitted.
 
 Theorem plus_n_Sm : forall n m : nat,
